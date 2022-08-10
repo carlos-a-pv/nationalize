97a4bb96adea518304faadbce3a06404c2a15363
Carlos Perez <carlopverano@gmail.com>
2022-08-10 16:22:19 -0500

    final project

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const buttonGo = document.getElementById('go');
 const inputName = document.getElementById('nombre')
 const results = document.getElementById('results');
+const firtsCountry = document.getElementById('firts_country');
+const secondCountry = document.getElementById('second_country');
+const thridCountry = document.getElementById('thrid_country');
 const google = 'https://www.google.com/?hl=es';
 // results.innerHTML = '';
 
@@ -21,24 +24,46 @@ buttonGo.addEventListener('click', ()=> {
         .then(response => response.json())
         .then(data => {
 	// debugger
-    //console.log(data);
-    //console.log(typeof(data.country));
     console.log(data);
-    console.log(typeof(data.country.length));
-    if (data.country === 0){
+    //console.log(data.country[0].country_id);
+    // console.log(data.country.length);
+    // console.log(data);
+    // console.log(typeof(data.country.length));
+    if (data.country.length === 0){
         console.log("No se encontraron resultados.");
+        let p = document.createElement("p");
+        p.textContent = "No se encontraron resultados.";
+        results.appendChild(p);
     }else{
-        for (const country of data.country) {
+        //for (const country of data.country) {
             // console.log(country.country_id);
-            let itemList = document.createElement('li');
-            itemList.textContent = country.country_id ;
-            results.appendChild(itemList);
-    
-            
-        }   
+            //let itemList = document.getElementById('firts_country');
+        //console.log(firtsCountry.getAttribute('href'));
+        firtsCountry.textContent = data.country[0].country_id ;
+        let firtsLink = firtsCountry.getAttribute('href'); 
+        firtsLink += `${firtsCountry.textContent}+pais`;
+        firtsCountry.setAttribute('href', firtsLink);
+        results.appendChild(firtsCountry);
+
+
+        secondCountry.textContent = data.country[1].country_id;
+        let secondLink = secondCountry.getAttribute('href') ;
+        secondLink += `${secondCountry.textContent}+pais`;
+        secondCountry.setAttribute('href', secondLink);
+        results.appendChild(secondCountry);
+
+        thridCountry.textContent = data.country[2].country_id;
+        let thirdLink = thridCountry.getAttribute('href') ;
+        thirdLink += `${thridCountry.textContent}+pais`;
+        thridCountry.setAttribute('href', thirdLink);
+        results.appendChild(thridCountry);
+        //}   
     }
         })
     }
+    firtsCountry.setAttribute('href', 'https://www.google.com/search?q=');
+    secondCountry.setAttribute('href', 'https://www.google.com/search?q=');
+    thridCountry.setAttribute('href', 'https://www.google.com/search?q=');
     inputName.value = '';
 })
 
